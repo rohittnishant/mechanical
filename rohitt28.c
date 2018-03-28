5203432e29b6cf8f0de9ad01817eb59b694c5705
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-03-28 15:40:47 +0530

    Create rohitt28.c

---
+++ rohitt28.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   char a[10];
+   int i,j,n,c=0;
+   scanf("%s",&a);
+   n=strlen(a);
+   for(i=0;i<n;i++)
+ {
+   for(j=i+1;j<n;j++)
+ {
+   if(a[i]==a[j])  
+ {
+   c=1;
+   break;
+   }
+    else
+  {  
+   continue;
+   }
+  }
+ }
+    if(c==0)
+ {
+  printf("YES");
+  }
+    else
+ {
+  printf("NO");
+  }
+  getch();
+	return 0;
+}
