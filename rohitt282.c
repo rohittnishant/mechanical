775ab438b8044e8a94bb0dd56e3dada99140f60d
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-03-28 15:43:38 +0530

    Create rohitt282.c

---
+++ rohitt282.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c,i,d;
+	scanf("%d %d",&a,&b);
+	for(i=1;(i<=a)&&(i<=b);i++)
+{
+	if((a%i==0) && (b%i==0))     
+{
+	c=i;
+ }
+}
+	d=(a*b)/c;
+	printf("%d",d);
+  getch();
+	return 0;
+}
